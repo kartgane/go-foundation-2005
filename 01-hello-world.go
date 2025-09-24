13a19b537dd040acc8477adaf1cbb26b8a71e2eb
Karthikeyan Ganesan <kartgane@cisco.com>
2025-09-24 16:38:44 +0530

    Add files via upload

---
+++ 01-hello-world.go
@@ -0,0 +1,21 @@
+/* package declaration (mandatory) */
+package main
+
+import "fmt"
+
+/* import dependency packages */
+
+/* package scoped variable / constant / type declarations */
+
+/* package init function */
+
+/* main function */
+func main() {
+
+	// print("Hello World!\n")
+	// println("Hello World!")
+
+	fmt.Println("Hello, World!")
+}
+
+/* other functions */
